994414bc6ad94b707079ca8a3839e045caef7b4e
CoderVinit <vinitpatel8896@gmail.com>
2024-09-29 12:01:34 +0530

    fronetned

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -30,7 +30,8 @@ const Login = () => {
     const config = {
       withCredentials: true,
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
       }
     }
 
