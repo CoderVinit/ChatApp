23875b96a20ad878c1200b72abe2e00b4405cb43
CoderVinit <vinitpatel8896@gmail.com>
2024-09-29 11:52:51 +0530

    backend

--- server/app.js
+++ server/app.js
@@ -62,6 +62,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors(corsOptions))
+app.options("*", cors(corsOptions));
 
 app.use("/api/v1/users", userRoutes)
 app.use("/api/v1/chats", chatRoutes)
--- server/constants/config.js
+++ server/constants/config.js
@@ -3,7 +3,6 @@ const corsOptions = {
   origin: ["http://localhost:5173", "http://localhost:4173", "https://chat-app-wft1.vercel.app", process.env.CLIENT_URL],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
-  useNewUrlParser: true, useUnifiedTopology: true
 }
 
 const TOKEN = 'token'
