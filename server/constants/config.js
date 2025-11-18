dc7bbe9cbfe1b8cabab8a08d13630980b1578761
VINIT KUMAR PATEL <101181057+CoderVinit@users.noreply.github.com>
2025-11-18 15:03:42 +0530

    update CORS options to include additional origin

--- server/constants/config.js
+++ server/constants/config.js
@@ -1,6 +1,6 @@
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:4173", "https://chat-app-wft1.vercel.app", process.env.CLIENT_URL],
+  origin: ["http://localhost:5173", "http://localhost:4173", "https://chat-app-wft1.vercel.app","https://chat-app-ten-ecru-78.vercel.app", process.env.CLIENT_URL],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }
